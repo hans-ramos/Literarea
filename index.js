95e57a201e5b8f1a5839f9ec6abeb47633653fdc
hans-ramos <jester_ramos@dlsu.edu.ph>
2020-08-31 16:58:24 +0800

    Added home page,login page, register page and css

--- index.js
+++ index.js
@@ -11,6 +11,37 @@ const urlencoder = bodyparser.urlencoded({
     extended:false
 })
 
+app.use(session({
+    secret: "very secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie:{
+        maxAge:1000*60*60
+    }
+}))
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", (req,res)=>{
+    if(req.session.username){
+        res.render("index.hbs",{
+            username:req.session.username
+        })
+    }
+    else{
+        res.render("index.hbs")
+    }
+})
+
+app.get("/register", (req,res)=>{
+    res.render("register.hbs")
+})
+
+app.get("/login", (req,res)=>{
+    res.render("login.hbs")
+})
+
+
 app.listen(3000, function(){
     console.log("now listening to port 3000")
 })
