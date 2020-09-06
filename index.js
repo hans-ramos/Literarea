0bf6beeb904d92eb6976d638736e6cae2fc7f07a
hans-ramos <jester_ramos@dlsu.edu.ph>
2020-09-06 19:23:33 +0800

    Installed mongoose, added views

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const session = require("express-session")
 const bodyparser = require("body-parser")
 const cookieparser = require("cookie-parser")
 const e = require("express")
+const { handlebars } = require("hbs")
 const hbs = ("hbs")
-
+const mongoose = require("mongoose")
 const app = express()
 
 const urlencoder = bodyparser.urlencoded({
@@ -29,18 +30,179 @@ app.get("/", (req,res)=>{
         })
     }
     else{
-        res.render("index.hbs")
+        res.render("index.hbs",{
+        })
     }
 })
 
 app.get("/register", (req,res)=>{
-    res.render("register.hbs")
+    if(!req.session.username)
+    {
+        res.render("register.hbs")
+    }
+    else{
+        res.redirect("/")
+    }
 })
 
 app.get("/login", (req,res)=>{
-    res.render("login.hbs")
+    if(!req.session.username)
+    {
+        res.render("login.hbs")
+    }
+    else{
+        res.redirect("/")
+    }
 })
 
+app.post("/login_form",urlencoder, (req,res)=>{
+    if(req.body.username=="admin"&&req.body.password=="1234"){
+        req.session.username = "admin"
+        res.redirect("/")
+    }
+    else{
+        res.render("login.hbs",{
+            error:"Incorrect username or password"
+        })
+    }
+})
+
+app.get("/userprofile", (req,res)=>{
+    if(!req.session.username){
+        res.redirect("/login")
+    }
+    else{
+        res.render("userprofile.hbs",{
+            username:req.session.username
+        })
+    }
+})
+
+app.get("/view_prompts",(req,res)=>{
+    if(req.session.username){
+        res.render("view_prompts.hbs",{
+            username:req.session.username
+        })
+    }
+    else{
+        res.render("view_prompts.hbs")
+    }
+})
+
+app.get("/view_stories",(req,res)=>{
+    if (req.session.username){
+        res.render("view_stories.hbs",{
+            username:req.session.username
+        })
+    }
+    else{
+        res.render("view_stories.hbs")
+    }
+})
+
+app.get("/subscriptions", (req,res)=>{
+    if(!req.session.username){
+        res.redirect("/login")
+    }
+    else{
+        res.render("subscriptions.hbs",{
+            username:req.session.username
+        })
+    }
+})
+
+app.get("/collection", (req,res)=>{
+    if(!req.session.username){
+        res.redirect("/login")
+    }
+    else{
+        res.render("collection.hbs",{
+            username:req.session.username
+        })
+    }
+})
+
+app.get("/get_profile",(req,res)=>{
+    if (req.session.username){
+        res.render("getprofile.hbs",{
+            username:req.session.username
+        })
+    }
+    else{
+        res.render("getprofile.hbs")
+    }
+})
+
+app.get("/post_story", (req,res)=>{
+    if(!req.session.username){
+        res.redirect("/login")
+    }
+    else{
+        res.render("post_story.hbs",{
+            username:req.session.username
+        })
+    }
+})
+
+app.get("/post_prompt", (req,res)=>{
+    if(!req.session.username){
+        res.redirect("/login")
+    }
+    else{
+        res.render("post_prompt.hbs",{
+            username:req.session.username
+        })
+    }
+})
+
+app.get("/edit_story", (req,res)=>{
+    if(!req.session.username){
+        res.redirect("/login")
+    }
+    else{
+        res.render("edit_story.hbs",{
+            username:req.session.username
+        })
+    }
+})
+
+app.get("/edit_prompt", (req,res)=>{
+    if(!req.session.username){
+        res.redirect("/login")
+    }
+    else{
+        res.render("edit_prompt.hbs",{
+            username:req.session.username
+        })
+    }
+})
+
+app.get("/read_story",(req,res)=>{
+    if (req.session.username){
+        res.render("read_story.hbs",{
+            username:req.session.username
+        })
+    }
+    else{
+        res.render("read_story.hbs")
+    }
+})
+
+app.get("/read_prompt",(req,res)=>{
+    if (req.session.username){
+        res.render("read_prompt.hbs",{
+            username:req.session.username
+        })
+    }
+    else{
+        res.render("read_prompt.hbs")
+    }
+})
+
+app.get("/signout",(req,res)=>{
+    req.session.destroy()
+    res.redirect("/")
+})
 
 app.listen(3000, function(){
     console.log("now listening to port 3000")
